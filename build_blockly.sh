ed31c3dd28020583f9a2f23434f4492e4be7a993
Paul Varache <perso@paulvarache.ninja>
2017-09-15 15:53:16 +0100

    Add missing blockly built

--- build_blockly.sh
+++ build_blockly.sh
@@ -5,6 +5,7 @@ CURRENT_DIR=$(pwd)
 
 # Remove the current blockly
 rm -rf blockly_built
+mkdir blockly_built
 
 cp -r blockly $TMP_DIR/blockly
 cp -r closure-library $TMP_DIR/closure-library
@@ -19,7 +20,17 @@ echo 'Building blockly'
 
 ./build.py
 
-mv $TMP_DIR/blockly $CURRENT_DIR/blockly_built
+mv $TMP_DIR/blockly/blockly_* $CURRENT_DIR/blockly_built/
+mkdir -p $CURRENT_DIR/blockly_built/msg/
+mkdir -p $CURRENT_DIR/blockly_built/media/
+mv $TMP_DIR/blockly/msg/**/* $CURRENT_DIR/blockly_built/msg/
+mv $TMP_DIR/blockly/media/* $CURRENT_DIR/blockly_built/media/
+mv $TMP_DIR/blockly/blocks_compressed.js $CURRENT_DIR/blockly_built/blocks_compressed.js
+mv $TMP_DIR/blockly/dart_compressed.js $CURRENT_DIR/blockly_built/dart_compressed.js
+mv $TMP_DIR/blockly/javascript_compressed.js $CURRENT_DIR/blockly_built/javascript_compressed.js
+mv $TMP_DIR/blockly/lua_compressed.js $CURRENT_DIR/blockly_built/lua_compressed.js
+mv $TMP_DIR/blockly/php_compressed.js $CURRENT_DIR/blockly_built/php_compressed.js
+mv $TMP_DIR/blockly/python_compressed.js $CURRENT_DIR/blockly_built/python_compressed.js
 
 echo 'Cleaning up'
 
