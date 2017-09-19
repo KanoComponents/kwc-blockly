e7cbc6135a875cb89c545902d6f1832b27334da0
Paul Varache <perso@paulvarache.ninja>
2017-09-19 15:25:30 +0100

    Use new custom color definition system

---
+++ override/core/block.js
@@ -0,0 +1,13 @@
+/**
+ * Get the colour of a block.
+ * @return {string} #RRGGBB string.
+ */
+Blockly.Block.prototype.getColour = function() {
+    if (this.isShadow()) {
+        let parent = this.getParent();
+        if (parent) {
+            return parent.getColour();
+        }
+    }
+    return this.customColor || this.colour_;
+};
