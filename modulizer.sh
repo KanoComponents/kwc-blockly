aef662e0b2d6b253f8b957ca57935c93f9a1da9e
Paul Varache <paul@kano.me>
2018-05-23 17:37:02 +0100

    Bump version

--- modulizer.sh
+++ modulizer.sh
@@ -1,6 +1,6 @@
 modulizer --out . \
 --npm-name @kano/kwc-blockly \
---npm-version 3.0.0-beta.1 \
+--npm-version 3.0.0-beta.2 \
 --dependency-mapping kwc-style,@kano/kwc-style,^3.0.0 \
 --dependency-mapping kwc-color-picker,@kano/kwc-color-picker,^3.0.0
 echo "window.goog = goog;window.Blockly=Blockly;" >> blockly_built/blockly_compressed.js
