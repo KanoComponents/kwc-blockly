15e2d6c743579df9c8b2fec31a8e74fb1dce2088
Paul Varache <perso@paulvarache.ninja>
2018-05-24 09:41:40 +0100

    Add missing from

--- modulizer.sh
+++ modulizer.sh
@@ -4,5 +4,5 @@ modulizer --out . \
 --dependency-mapping kwc-style,@kano/kwc-style,^3.0.0-beta.1 \
 --dependency-mapping kwc-color-picker,@kano/kwc-color-picker,^3.0.0-beta.2
 echo "window.goog = goog;window.Blockly=Blockly;" >> blockly_built/blockly_compressed.js
-sed -i "s|import '../../@kano/kwc-color-picker/palettes/material.js'|import { Material } '../../@kano/kwc-color-picker/palettes/material.js'|g" import.js
+sed -i "s|import '../../@kano/kwc-color-picker/palettes/material.js'|import { Material } from '../../@kano/kwc-color-picker/palettes/material.js'|g" import.js
 sed -i "s|KwcColorPickerPalette.Material'|Material'|g" import.js
