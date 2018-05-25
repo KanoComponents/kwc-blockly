18397e62faf12e1d7c00d47c9c2767a3d68849d8
Paul Varache <paul@kano.me>
2018-05-25 16:50:51 +0100

    Update version

--- modulizer.sh
+++ modulizer.sh
@@ -1,6 +1,6 @@
 modulizer --out . \
 --npm-name @kano/kwc-blockly \
---npm-version 3.0.0-beta.5 \
+--npm-version 3.0.0-beta.6 \
 --dependency-mapping kwc-style,@kano/kwc-style,^3.0.0-beta.1 \
 --dependency-mapping kwc-color-picker,@kano/kwc-color-picker,^3.0.0-beta.3 \
 --import-style name
