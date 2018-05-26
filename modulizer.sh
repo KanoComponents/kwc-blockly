b5b43c65f3e2043c262a5de0718f8f4179f46fd2
Paul Varache <perso@paulvarache.ninja>
2018-05-26 17:07:52 +0100

    Bump version

--- modulizer.sh
+++ modulizer.sh
@@ -1,9 +1,9 @@
 modulizer --out . \
 --npm-name @kano/kwc-blockly \
---npm-version 3.0.0-beta.7 \
+--npm-version 3.0.0-beta.8 \
 --dependency-mapping kwc-style,@kano/kwc-style,^3.0.0-beta.2 \
 --dependency-mapping kwc-color-picker,@kano/kwc-color-picker,^3.0.0-beta.4 \
 --import-style name
 echo "window.goog = goog;window.Blockly=Blockly;" >> blockly_built/blockly_compressed.js
 sed -i "s|import '@kano/kwc-color-picker/palettes/material.js'|import { Material } from '@kano/kwc-color-picker/palettes/material.js'|g" import.js
-sed -i "s|KwcColorPickerPalette.Material'|Material'|g" import.js
+sed -i "s|KwcColorPickerPalette.Material|Material|g" import.js
