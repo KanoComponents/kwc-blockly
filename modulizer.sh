6b73840b151661961af0b06d21d80a857c31d695
Paul Varache <paul@kano.me>
2018-06-07 14:33:47 +0100

    Reload assets when pathToMedia changes

--- modulizer.sh
+++ modulizer.sh
@@ -1,6 +1,6 @@
 modulizer --out . \
 --npm-name @kano/kwc-blockly \
---npm-version 3.0.0-beta.10 \
+--npm-version 3.0.0-beta.11 \
 --dependency-mapping kwc-style,@kano/kwc-style,^3.0.0-beta.2 \
 --dependency-mapping kwc-color-picker,@kano/kwc-color-picker,^3.0.0-beta.4 \
 --import-style name
