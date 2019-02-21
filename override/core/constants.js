90d5f81d135bc40c9ea16764cabcb6199f20f2e2
Radek Pazdera <radek@kano.me>
2019-02-21 18:28:20 +0000

    Fixing our overrides to work with the new blockly version

---
+++ override/core/constants.js
@@ -0,0 +1,7 @@
+/**
+ * String for use in the dropdown created in field_variable.
+ * This string indicates that this option in the dropdown is 'New
+ * variable...' and if selected, should trigger the prompt to create a variable.
+ * @const {string}
+ */
+Blockly.NEW_VARIABLE_ID = 'NEW_VARIABLE_ID';
