961dd288b455672641d2871851ca0094e58a8b3f
Paul Varache <paul@kano.me>
2019-05-21 12:19:14 +0100

    Fix else checkbox connection issue

--- field/if-stack.js
+++ field/if-stack.js
@@ -2,23 +2,23 @@ import { goog, Blockly } from '../blockly-module.js';
 import './config-input.js';
 import '../input/kwc-blockly-if-stack.js';
 
-const FieldIfStack = function (value, opt_validator) {
+export const FieldIfStack = function FieldIfStack(value, opt_validator) {
     this._width = 16;
     this._height = 16;
     FieldIfStack.superClass_.constructor.call(this, value || 0, opt_validator);
 };
 goog.inherits(FieldIfStack, Blockly.FieldConfig);
 
-FieldIfStack.prototype.showEditor_ = function () {
+FieldIfStack.prototype.showEditor_ = function showEditor_() {
     Blockly.WidgetDiv.show(this, this.sourceBlock_.RTL, FieldIfStack.widgetDispose_);
 
     const div = Blockly.WidgetDiv.DIV;
 
     this.customEl = document.createElement('kwc-blockly-if-stack');
-    this.customEl.value = this.getValue();
+    this.customEl.value = JSON.parse(this.getValue());
 
-    this.customEl.addEventListener('value-changed', (e) => {
-        this.setValue(this.customEl.value);
+    this.customEl.addEventListener('value-changed', () => {
+        this.setValue(JSON.stringify(this.customEl.value));
     });
 
     div.appendChild(this.customEl);
@@ -36,3 +36,5 @@ FieldIfStack.prototype.showEditor_ = function () {
 };
 
 window.Blockly.FieldIfStack = FieldIfStack;
+
+export default FieldIfStack;
